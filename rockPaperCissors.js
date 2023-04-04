809bd23b29db007c61debbd50a63622bf03a4bbf
nils <26379736+NiGour@users.noreply.github.com>
2023-04-04 16:31:41 +0200

    the script executes 5 rounds

--- rockPaperCissors.js
+++ rockPaperCissors.js
@@ -10,44 +10,63 @@ function rockPaperOrScissors(choice){
         return "scissors";
     }
 }
-
-
 function getComputerChoice(){
 return rockPaperOrScissors(Math.floor(Math.random() * 3));
 }
 
+
 function playerSelection(){
-    let selection= prompt("choose :");
+    let selection="";
+    while(selection!="rock"&&selection!="paper"&&selection!="scissors"){
+    selection= prompt("choose :");
+    // selection shall be case insensitive
     selection=selection.toLowerCase();
-    return selection
+    }
+    return selection;
 
 }
 
+function endOfGame(c,p){
+    return(c+" beats "+p);
+}
+
+
+
+//c stands for computer selection, p stands for player selection
 function round(c,p){
     console.log("Computer choice :"+c);
     console.log("Your choice :"+p);
+
+    loss="you lose: " + endOfGame(c,p);
+    win="you win: " + endOfGame(p,c);
 
     if(c==p){
         console.log("draw");
         return;
     }
     if(c=="scissors" && p=="paper"){
-    console.log("you lose");
+    console.log(loss);
     return;
     }
     if(c=="rock" && p=="scissors"){
-    console.log("you lose");
+    console.log(loss);
     return;
     }
     if(c=="paper" && p=="rock"){
-    console.log("you lose");
+    console.log(loss);
     return;
     }
-    console.log("you win");
+    //if the loss or draw conditions aren't met, then the player wins
+    console.log(win);
     return;
-
 }
 
+function game(){
+    for(i=0;i<5;i++){
+        console.log("round "+(i+1));
+        round(getComputerChoice(),playerSelection());
+        console.log("");
+    }
+}
 
-
-round(getComputerChoice(),playerSelection());
+game();
