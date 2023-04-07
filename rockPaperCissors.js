c6280df0ff41effbeb8acc0fb89a1e906edbf2d8
nils <26379736+NiGour@users.noreply.github.com>
2023-04-07 07:22:41 +0200

    added a clickable interface

--- rockPaperCissors.js
+++ rockPaperCissors.js
@@ -1,4 +1,3 @@
-
 function rockPaperOrScissors(choice){
     if (choice==0){
         return "rock";
@@ -15,16 +14,6 @@ return rockPaperOrScissors(Math.floor(Math.random() * 3));
 }
 
 
-function playerSelection(){
-    let selection="";
-    while(selection!="rock"&&selection!="paper"&&selection!="scissors"){
-    selection= prompt("choose :");
-    // selection shall be case insensitive
-    selection=selection.toLowerCase();
-    }
-    return selection;
-
-}
 
 function endOfGame(c,p){
     return(c+" beats "+p);
@@ -34,39 +23,90 @@ function endOfGame(c,p){
 
 //c stands for computer selection, p stands for player selection
 function round(c,p){
-    console.log("Computer choice :"+c);
-    console.log("Your choice :"+p);
+    affichage.textContent+=("Computer choice :"+c+"\n");
+    affichage.textContent+=("Your choice :"+p+"\n");
 
-    loss="you lose: " + endOfGame(c,p);
-    win="you win: " + endOfGame(p,c);
+    loss="you lose: " + endOfGame(c,p)+"\n";
+    win="you win: " + endOfGame(p,c)+"\n";
 
     if(c==p){
-        console.log("draw");
+        affichage.textContent+=("draw\n");
         return;
     }
     if(c=="scissors" && p=="paper"){
-    console.log(loss);
+    affichage.textContent+=(loss);
+    computerScore++;
     return;
     }
     if(c=="rock" && p=="scissors"){
-    console.log(loss);
+    affichage.textContent+=(loss);
+    computerScore++;
     return;
     }
     if(c=="paper" && p=="rock"){
-    console.log(loss);
+    affichage.textContent+=(loss);
+    computerScore++;
     return;
     }
     //if the loss or draw conditions aren't met, then the player wins
-    console.log(win);
+    affichage.textContent+=(win);
+    playerScore++;
     return;
 }
+ 
 
-function game(){
-    for(i=0;i<5;i++){
-        console.log("round "+(i+1));
-        round(getComputerChoice(),playerSelection());
-        console.log("");
+i=0;
+computerScore=0;
+playerScore=0;
+
+const affichage=document.querySelector("div");
+affichage.Color="red";
+affichage.textContent="Pick one";
+affichage.style.border="solid 1px black";
+
+
+
+const buttons=document.querySelectorAll("button");
+buttons.forEach(button=>
+button.addEventListener("click",function displayClicked(){
+    affichage.textContent=("round "+(i+1)+"\n");
+
+    function playerSelection(){
+        selection= button.textContent;
+        return selection;
+    
     }
-}
+    
+    
+    round(getComputerChoice(),playerSelection());
 
-game();
+    affichage.textContent+=("computer score :"+computerScore+"\n");
+    affichage.textContent+=("your score :"+playerScore+"\n");
+    i+=1;
+    if(computerScore>4||playerScore>4){
+        affichage.textContent+=("GAME OVER\n");
+        if(computerScore>4){
+            affichage.textContent+="The computer won\n";
+        }
+        else{
+            affichage.textContent+="You won\n";
+        }
+        affichage.textContent+=("pick an option to play again");
+        i=0;
+        playerScore=0;
+        computerScore=0;
+    }
+
+
+
+
+}));
+
+//buttons.forEach(button=>
+    //button.addEventListener("click",round(getComputerChoice(),playerSelection()))
+
+    //);
+
+
+
+//game();
